720726934ee32dc9968234ff2ba0a543787e39a8
Felipe Figueiredo <philsf79@gmail.com>
2023-02-04 00:32:02 -0300

    prototype: new tab_app, all tables saved for performance

--- scripts/describe.R
+++ scripts/describe.R
@@ -27,12 +27,16 @@ theme_gtsummary_compact()
 
 # tables ------------------------------------------------------------------
 
-tab_desc <- analytical %>%
-  tbl_summary(
-    include = -id,
-    # by = exposure,
-  ) %>%
-  # modify_caption(caption = "**Tabela 1** Características demográficas") %>%
-  # modify_header(label ~ "**Características dos pacientes**") %>%
-  bold_labels() %>%
-  modify_table_styling(columns = "label", align = "center")
+# tab_desc <- analytical %>%
+#   tbl_summary(
+#     include = -id,
+#     # by = exposure,
+#   ) %>%
+#   # modify_caption(caption = "**Tabela 1** Características demográficas") %>%
+#   # modify_header(label ~ "**Características dos pacientes**") %>%
+#   bold_labels() %>%
+#   modify_table_styling(columns = "label", align = "center")
+# 
+# write_rds(tab_desc, "dataset/tab_desc.rds")
+
+tab_desc <- read_rds("dataset/tab_desc.rds")
--- scripts/modeling.R
+++ scripts/modeling.R
@@ -23,9 +23,9 @@ mod.late <- coxph(Surv(Time/dyears(1), outcome) ~ exposure + ., filter(md, Time 
 
 # tab_inf <- tbl_merge(
 #   tbls = list(
-#     mod.crude %>% tbl_regression(exp = TRUE) %>% bold_labels() %>% bold_p(), # crude HR
-#     mod.full %>% tbl_regression(exp = TRUE) %>% bold_labels() %>% bold_p(), # aHR
-#     mod.late %>% tbl_regression(exp = TRUE) %>% bold_labels() %>% bold_p() # Late deaths
+#     mod.crude %>% tbl_regression(exp = TRUE, include = exposure) %>% bold_labels() %>% bold_p(), # crude HR
+#     mod.full %>% tbl_regression(exp = TRUE, include = exposure) %>% bold_labels() %>% bold_p(), # aHR
+#     mod.late %>% tbl_regression(exp = TRUE, include = exposure) %>% bold_labels() %>% bold_p() # Late deaths
 #     ),
 #   tab_spanner = c("Crude estimate", "Adjusted estimate", "Late deaths")
 #   )
@@ -40,3 +40,16 @@ tab_inf <- read_rds("dataset/tab_inf.rds")
 # tab_inf %>%
 #   as_gt() %>%
 #   gtsave("table2.png")
+
+# tab_app <- tbl_merge(
+#   tbls = list(
+#     mod.crude %>% tbl_regression(exp = TRUE) %>% bold_labels() %>% bold_p(), # crude HR
+#     mod.full %>% tbl_regression(exp = TRUE) %>% bold_labels() %>% bold_p(), # aHR
+#     mod.late %>% tbl_regression(exp = TRUE) %>% bold_labels() %>% bold_p() # Late deaths
+#   ),
+#   tab_spanner = c("Crude estimate", "Adjusted estimate", "Late deaths")
+# )
+# 
+# write_rds(tab_app, "dataset/tab_app.rds")
+
+tab_app <- read_rds("dataset/tab_app.rds")
