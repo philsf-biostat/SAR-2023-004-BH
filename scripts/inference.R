7148a719dc49ae01bb6868e986b9b73378adf12e
Felipe Figueiredo <philsf79@gmail.com>
2023-06-14 14:41:25 -0300

    saved objects renamed

--- scripts/inference.R
+++ scripts/inference.R
@@ -5,5 +5,5 @@
 
 # tables are created in tables-save.R and saved to disk. They are loaded here
 
-tab_inf <- read_rds("dataset/tab_inf.rds")
-tab_app <- read_rds("dataset/tab_app.rds")
+tab_inf <- read_rds("dataset/tab_inf_004.rds")
+tab_app <- read_rds("dataset/tab_app_004.rds")
